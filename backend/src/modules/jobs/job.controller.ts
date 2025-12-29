0d6ea4a44e16b6058ac1f1188ca7d82d4e12572d
Yashh-tripathi <yashtripathi980@gmail.com>
2025-12-30 00:03:53 +0530

    Implement job creation endpoint with error handling

---
+++ backend/src/modules/jobs/job.controller.ts
@@ -0,0 +1,32 @@
+import {Request, Response } from "express";
+import ApiError from "../../utils/APiError/apiError";
+import { Job } from "./job.model";
+import ApiResponse from "../../utils/Apiresponse/apiResponse";
+
+
+
+//create job 
+export const createJob = async (request:Request, response: Response) => {
+
+    try {
+        const { title, descriptionText, requiredSkills, minExperience, embedding } = request.body;
+
+        if(!title){
+            throw new ApiError(400, "Job title is required")
+        }
+
+        const job = await Job.create({
+            title,
+            descriptionText,
+            requiredSkills,
+            minExperience,
+            embedding
+        });
+
+        return response.status(201).json(new ApiResponse(201, job, "job uploaded successfully"))
+    } catch (error) {
+        return response.status(500).json(new ApiResponse(500, null, "Failed to craete job"))
+    }
+}
+
+
